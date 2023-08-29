246132809bc714005b69b68b4eae50411e58e471
Malkevich Liliya E <MalkevichLE@bsu.by>
2023-08-29 15:43:53 +0300

    fix:task done

--- src/index.js
+++ src/index.js
@@ -39,7 +39,30 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let res = [];
+    let morseArr = [];
+    let str = '';
+    let dec = '';
+  for (let i = 0; i < expr.length; i += 10) {
+      const chunk = expr.slice(i, i + 10);
+      res.push(chunk);
+  }
+  res.forEach(a=>{
+    str=a.replace(/10/gi, '.');
+    str=str.replace(/11/gi, '-');
+    str=str.replace(/0/gi, '');
+    morseArr.push(str) 
+    })
+  morseArr.forEach(a=>{
+    let el = Object.values(MORSE_TABLE)[Object.keys(MORSE_TABLE).indexOf(a)]
+    if(el != undefined) dec+= el;
+    else dec+= ' ';
+  })
+  return dec;
 }
+
+
+console.log(decode('0000101110000011111100000010110000111010**********00000000110000111111**********00101111100000101110000011111100001111100000101110000000101100000011110000001111000000101000000011100000111110'))
 
 module.exports = {
     decode
